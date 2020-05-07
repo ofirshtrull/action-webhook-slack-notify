9718c3ed4d550bc76f36ada422f20805f143682f
Cesar Bonilla <cefeboru@gmail.com>
2020-05-07 17:02:42 -0600

    Rebranding

--- main.go
+++ main.go
@@ -114,7 +114,7 @@ func main() {
 				AuthorName: envOr(EnvGithubActor, ""),
 				AuthorLink: "http://github.com/" + os.Getenv(EnvGithubActor),
 				AuthorIcon: "http://github.com/" + os.Getenv(EnvGithubActor) + ".png?size=32",
-				Footer: "<https://github.com/rtCamp/github-actions-library|Powered By rtCamp's GitHub Actions Library>",
+				Footer: "<https://github.com/partnerhero/github-actions-library|Powered By PartnerHero's GitHub Actions Library>",
 				Fields: fields,
 			},
 		},
--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 export GITHUB_BRANCH=${GITHUB_REF##*heads/}
 export SLACK_ICON=${SLACK_ICON:-"https://avatars0.githubusercontent.com/u/43742164"}
-export SLACK_USERNAME=${SLACK_USERNAME:-"rtBot"}
+export SLACK_USERNAME=${SLACK_USERNAME:-"DevOps Bot"}
 export CI_SCRIPT_OPTIONS="ci_script_options"
 export SLACK_TITLE=${SLACK_TITLE:-"Message"}
 export COMMIT_MESSAGE=$(cat "/github/workflow/event.json" | jq .commits | jq '.[0].message' -r)
